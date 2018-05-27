6ad8a785340a44842e139b1c7685b03f51cdfa72
subhamkrai <subhamkumarrai03@gmail.com>
2018-05-27 15:01:30 +0530

    youtube video by python

---
+++ python_vlc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+#import pafy
+#import vlc
+import os
+import urllib.request
+import urllib.parse
+import re
+
+query_string = urllib.parse.urlencode({"search_query" : input()})
+html_content = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+url="https://www.youtube.com/watch?v=" + search_results[0]
+print(url)
+
+os.system("vlc "+url)
+
+'''
+video = pafy.new(url)
+best = video.getbest()
+playurl = best.url
+
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+Media = Instance.media_new(playurl)
+Media.get_mrl()
+player.set_media(Media)
+player.play()
+'''	
